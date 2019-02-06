f2fe6176d579d7ce375e84d5822248780234af71
Abdullah Al Mohin Jaki <me.jaki@outlook.com>
2019-02-06 13:21:13 +0600

    crawl all links using selenium module

--- books_crawler/books_crawler/spiders/books.py
+++ books_crawler/books_crawler/spiders/books.py
@@ -1,14 +1,55 @@
 # -*- coding: utf-8 -*-
+from time import sleep
+from scrapy import Spider
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from scrapy.selector import Selector
+from scrapy.http import Request
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 
-class BooksSpider(CrawlSpider):
+class BooksSpider(Spider):
     name = 'books'
     allowed_domains = ['books.toscrape.com']
-    start_urls = ['http://books.toscrape.com/']
 
-    rules = (Rule(LinkExtractor(), callback='parse_page', follow=False),)
+    def start_requests(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get('http://books.toscrape.com')
 
-    def parse_page(self, response):
-        yield {'URL': response.url}
+        sel = Selector(text=self.driver.page_source)
+        books = sel.xpath('//h3/a/@href').extract()
+        for book in books:
+            url = 'http://books.toscrape.com/' + book
+            yield Request(url, callback=self.parce_book)
+
+        while True:
+            try:
+                next_page = self.driver.find_element_by_xpath('//a[text()="next"]')
+                sleep(3)
+                self.logger.info('Sleeping for 3 Seconds.')
+                next_page.click()
+
+                sel = Selector(text=self.driver.page_source)
+                books = sel.xpath('//h3/a/@href').extract()
+                for book in books:
+                    url = 'http://books.toscrape.com/catalogue/' + book
+                    yield Request(url, callback=self.parce_book)
+            except NoSuchElementException:
+                self.logger.info('No more pages to load.')
+                self.driver.quit()
+                break
+
+    def parce_book(self, response):
+        pass
+
+
+# class BooksSpider(CrawlSpider):
+#     name = 'books'
+#     allowed_domains = ['books.toscrape.com']
+#     start_urls = ['http://books.toscrape.com/']
+#
+#     rules = (Rule(LinkExtractor(), callback='parse_page', follow=False),)
+#
+#     def parse_page(self, response):
+#         yield {'URL': response.url}
